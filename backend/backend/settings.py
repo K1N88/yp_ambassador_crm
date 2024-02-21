1ac1b528d508ee5647fe7265c6407149fd04552f
K1N88 <m-end-eleev@yandex.ru>
2024-02-21 23:04:38 +0300

    add docs

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -150,7 +150,7 @@ AUTH_USER_MODEL = 'users.CrmUser'
 
 REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
-        'rest_framework.permissions.AllowAny',
+        'rest_framework.permissions.IsAuthenticated',
     ],
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework.authentication.TokenAuthentication',
