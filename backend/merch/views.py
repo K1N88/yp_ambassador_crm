b61ffe9bf7b9e9431a5f38756644b49aa1664592
Alex <ingaksdr@gmail.com>
2024-02-23 15:43:01 +0300

    Add view

--- backend/merch/views.py
+++ backend/merch/views.py
@@ -1,3 +1,7 @@
-from django.shortcuts import render
+from rest_framework import filters, mixins, status, viewsets
+from .models import MerchForSend
+from .serializers import MerchSerializer
 
-# Create your views here.
+class MerchandiseView(viewsets.GenericViewSet, mixins.ListModelMixin):
+    queryset = MerchForSend.objects.all()
+    serializer_class = MerchSerializer
