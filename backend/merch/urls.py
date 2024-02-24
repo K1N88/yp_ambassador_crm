aeb9df0c0a1ad229476402aeb4222e699fd4a13a
K1N88 <m-end-eleev@yandex.ru>
2024-02-24 21:10:02 +0300

    fix

--- backend/merch/urls.py
+++ backend/merch/urls.py
@@ -3,7 +3,8 @@ from django.urls import path
 from .views import MerchandiseView, SetStatusView
 
 urlpatterns = [
-    path('api/merchandise', MerchandiseView.as_view(), name='merchandise'),
-    path('ambassador/<int:ambassadorId>/merchandise/<int:merchandiseId>/state',
+    path('api/merchandise', MerchandiseView.as_view({'get': 'list'}),
+         name='merchandise'),
+    path('api/ambassadors/<int:ambassadorId>/merchandise/<int:merchandiseId>/state',  # noqa
          SetStatusView.as_view(), name='setstatus')
 ]
