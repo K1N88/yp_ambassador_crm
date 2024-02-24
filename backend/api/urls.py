619ebb63068bbcac00d81ac09d754d856ff6343c
K1N88 <m-end-eleev@yandex.ru>
2024-02-24 20:13:44 +0300

    add ambassadors model6

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,12 +1,15 @@
 from django.urls import include, path
 from rest_framework.routers import DefaultRouter
 
-from api.views import AmbassadorsViewSet
+from api.views import (AmbassadorsViewSet, StudyProgrammViewSet,
+                       SupervisorViewSet)
 
 
 router = DefaultRouter()
 
 router.register('ambassadors', AmbassadorsViewSet)
+router.register('supervisors', StudyProgrammViewSet)
+router.register('study_programms', SupervisorViewSet)
 
 urlpatterns = [
     path('auth/', include('djoser.urls.authtoken')),
