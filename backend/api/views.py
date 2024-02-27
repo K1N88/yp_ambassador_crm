869516ec9726250f029c23729929ccae40100faf
Natolin Artem <arnat64@yandex.ru>
2024-02-27 19:18:25 +0300

    small fix

--- backend/api/views.py
+++ backend/api/views.py
@@ -149,8 +149,6 @@ class ContentViewSet(
     def get_serializer_class(self):
         if self.request.method in SAFE_METHODS:
             return ContentListSerializer
-        elif self.request.method in ['PUT', 'PATCH']:
-            return ContentUpdateSerializer  # Доб. update в сер. ниже?
         return ContentPostSerializer
 
     def get_queryset(self):
